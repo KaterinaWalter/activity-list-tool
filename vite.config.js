f7eb9fc5aa9c61d4d5b07ec1638a1b672eceae70
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2026-05-14 17:38:14 +0000

    base url update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/activity-list-tool/",
+  base: "/activities-tool/",
   plugins: [react()],
   test: {
     globals: true,
